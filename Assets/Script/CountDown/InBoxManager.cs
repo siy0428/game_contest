37f82849322da993b8624d08c464d84ac5dc1b15
siy0428 <baske_r4_5g@yahoo.co.jp>
2021-05-01 18:29:00 +0900

    出撃時の行動範囲の追加

--- Assets/Script/CountDown/InBoxManager.cs
+++ Assets/Script/CountDown/InBoxManager.cs
@@ -7,16 +7,43 @@ public class InBoxManager : MonoBehaviour
     [SerializeField]
     private GameObject[] InBoxs;
     [SerializeField]
+    private GameObject RangeBox;
+    [SerializeField]
     private CountDownManager Timer;
+
+    private Color DefaultColor;
+    private float Alpha;
+    private bool DecAlpha;
 
     void Start()
     {
-        
+        //初期化
+        DefaultColor = RangeBox.GetComponent<Renderer>().material.GetColor("_Color");
+        Alpha = 0.5f;
+        DecAlpha = false;
+
+        Create();
+        Timer.CountStart();
     }
 
     void Update()
     {
+        //行動範囲の透明度調整
+        RangeBox.GetComponent<Renderer>().material.SetColor("_Color", DefaultColor);
+        DefaultColor.a = Alpha;
         
+        //一定時間経過処理
+        TimeUp(3.0f);
+
+        //一定時間経過後行動範囲を徐々に透明化
+        if(DecAlpha)
+        {
+            Alpha -= 0.01f;
+            if (Alpha <= 0.0f)
+            {
+                DecAlpha = false;
+            }
+        }
     }
 
     /// <summary>
@@ -28,6 +55,7 @@ public class InBoxManager : MonoBehaviour
         {
             box.SetActive(true);
         }
+        DecAlpha = false;
     }
 
     /// <summary>
@@ -38,6 +66,21 @@ public class InBoxManager : MonoBehaviour
         foreach (GameObject box in InBoxs)
         {
             box.SetActive(false);
+        }
+        DecAlpha = true;
+    }
+
+    /// <summary>
+    /// 一定時間経過処理
+    /// </summary>
+    /// <param name="limit">何秒間後に実行するか指定</param>
+    private void TimeUp(float limit)
+    {
+        if (Timer.GetTime > limit)
+        {
+            Timer.CountStop();
+            Timer.CountReset();
+            Delete();
         }
     }
 }
