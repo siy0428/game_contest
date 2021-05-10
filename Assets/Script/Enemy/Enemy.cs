9616b323fa17e03bcb1db79a817f62536b4ac9ed
siy0428 <baske_r4_5g@yahoo.co.jp>
2021-05-10 18:07:26 +0900

    ループ処理追加

--- Assets/Script/Enemy/Enemy.cs
+++ Assets/Script/Enemy/Enemy.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private bool IsAlive;
-
     // Start is called before the first frame update
     void Start()
     {
-        IsAlive = true;
+        
     }
 
     // Update is called once per frame
