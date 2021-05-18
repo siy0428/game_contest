9f8c86976c6aafe430f98e8abc2012409fc7cae7
siy0428 <53132787+siy0428@users.noreply.github.com>
2021-05-18 16:21:11 +0900

    Revert "Hagi branch"

--- Assets/Script/UIData/ScoreController.cs
+++ Assets/Script/UIData/ScoreController.cs
@@ -3,32 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-//-------------------------------------------------------
-//‚Ü‚¾–¢ŽÀ‘•AƒXƒRƒA‘‚â‚·ˆ—‚ð“G‚ð“|‚·ˆ—‚ÌŠ‚ÉŽÀ‘•‚·‚é
-//-------------------------------------------------------
+public class ScoreController : MonoBehaviour
+{
+    int score = 0;
+    Text textComponent;
 
-//public class ScoreController : MonoBehaviour
-//{
-//    int score = 0;
-//    Text textComponent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.textComponent = GameObject.Find("score").GetComponent<Text>();
+        this.textComponent.text = "�X�R�A " + score.ToString();
+    }
 
+    // Update is called once per frame
+    void Update()
+    {
 
-//    // Start is called before the first frame update
-//    void Start()
-//    {
-//        this.textComponent = GameObject.Find("Text").GetComponent<Text>();
-//        this.textComponent.text = "ƒXƒRƒA " + score.ToString();
-//    }
+    }
 
-//    // Update is called once per frame
-//    void Update()
-//    {
-
-//    }
-
-//    public void AddScore()
-//    {
-//        this.score += 10;
-//        this.textComponent.text = "ƒXƒRƒA " + score.ToString();
-//    }
-//}
+    public void AddScore()
+    {
+        this.score += 10;
+        this.textComponent.text = "�X�R�A " + score.ToString();
+    }
+}
