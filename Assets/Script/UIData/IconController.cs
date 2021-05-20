ebd3d6a4e95b1753266f3768e793fee08b3867e4
Noa <Noa@WIN10THS80276>
2021-05-20 10:49:01 +0900

    プルするためのコミット

--- Assets/Script/UIData/IconController.cs
+++ Assets/Script/UIData/IconController.cs
@@ -8,23 +8,51 @@ using UnityEngine.UI;
 public class IconController : MonoBehaviour
 {
     [SerializeField]
-    private Canvas canvas;
+    private Canvas canvas = new Canvas();
+
     [SerializeField]
     private Transform targetTfm;
 
     private RectTransform canvasRectTfm;
     private RectTransform myRectTfm;
-    private Vector3 offset = new Vector3(0, 70.0f, 0);  //アイコンの座標調整に使う
+    private Vector3 offset = new Vector3(0, 0.5f, 0);  //アイコンの座標調整に使う
+
+
+    //操作しているキャラクターが死んだか
+    private bool IsDead = false;
+
+    //プレイヤーリスト
+    public List<GameObject> Players = new List<GameObject>();
+
+    //取得したキャラクターのデータ
+    public List<Player> PlayersData = new List<Player>();
+
 
     void Start()
     {
         canvasRectTfm = canvas.GetComponent<RectTransform>();
         myRectTfm = GetComponent<RectTransform>();
+        //初期化
+        for (int i = 0; i < Players.Count; i++)
+        {
+            PlayersData.Add(Players[i].GetComponent<Player>());//キャラクターデータ取得
+            PlayersData[i].PlayerID = i;
+            //if (i == ControlPlayerID)
+            //{
+            //    Players[i].GetComponent<SpriteRenderer>().sortingOrder = 1;
+            //}
+            //else
+            //{
+            //    Players[i].GetComponent<SpriteRenderer>().sortingOrder = 0;
+            //}
+        }
+
     }
 
     void Update()
     {
         Vector2 pos;
+
 
         switch (canvas.renderMode)
         {
@@ -45,6 +73,21 @@ public class IconController : MonoBehaviour
                 myRectTfm.LookAt(Camera.main.transform);
 
                 break;
+        }
+
+        //全てのプレイヤーの動きを更新する
+        for (int i = 0; i < Players.Count; i++)
+        {
+            if (!PlayersData[i].IsAlive)
+            {
+
+            }
+            if (IsDead)
+            {
+                PlayersData[i].IsAlive = false;
+                IsDead = false;
+            }
+
         }
     }
 }
